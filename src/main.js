6754592d16a27dc189cdbe1e3dafef8cd4abc7bb
WuZetong <932361223@qq.com>
2020-09-25 16:10:28 +0800

    kRouter

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import create from '@/utils/create.js'
-import router from './router'
+import router from './krouter'
 
 Vue.config.productionTip = false
 // Vue.prototype.$create=create
